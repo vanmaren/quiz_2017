d902bdbdccd9913be1573801ab7408705deac5d8
Ester Vicario <estervicario@MacBook-Pro-de-Ester.local>
2017-05-27 00:09:23 +0200

    intento

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -188,54 +188,86 @@ exports.check = function (req, res, next) {
     });
 };
 
-// GET /quizzes/randomcheck/:quizId?answer=respuesta
+// GET /quizzes/random_play
+exports.randomplay = function (req, res, next) {
+
+    if(!req.session.p52){
+
+        req.session.p52={pyp:[-1]};
+    }
+
+    models.Quiz.count({where:{id:{$notIn:req.session.p52.pyp}}})
+        .then(function (count) {
+            if(!req.session.p52.max){
+                req.session.p52.max=count;
+            }
+            if(req.session.p52.pyp.length-1 == req.session.p52.max){
+                // var empty=[];
+                var score = req.session.pyp.length-1;
+                req.session.p52=null;
+                req.session.p52.max=null;
+                //destroy(req.session.p52.sessionID);
+
+                // session_destroy();
+                //req.session.p52.pyp.length =0;
+                // res.render('quizzes/randomnomore',{score: score });
+                next();
+
+            }
+            else{
+                var aleatoria = Math.floor(Math.random()*count);
+                return models.Quiz.findAll({where:{id:{$notIn:req.session.p52.pyp}},limit:1,offset:aleatoria});
+            }
+        })
+        .then(function (quizzes) {
+            var q = quizzes[0];
+            // var score= req.session.p52.pyp -1;
+            res.render('quizzes/randomplay',{quiz:q,
+                score:req.session.p52.pyp.length-1} );
+
+        })
+        .catch(function (error) {
+
+            res.render('quizzes/randomnomore',{score: req.session.p52.pyp.length-1 });
+
+        })
+
+
+};
+
+// GET /quizzes/randomcheck
 exports.randomcheck = function (req, res, next) {
 
     var answer = req.query.answer || "";
 
     var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
 
-    res.render('quizzes/random_result', {
-        quiz: req.quiz,
-        result: result,
-        answer: answer
-    });
+    if(!result){
 
-};
-// GET /quizzes/randomplay
-exports.randomplay = function (req, res, next) {
-    if(!req.session.p52){
-        req.session.p52={resolved:[-1]};
+        var score= req.session.p52.pyp-1;
+
+        res.render('quizzes/randomresult', {
+            quiz: req.quiz,
+            result: result,
+            answer: answer,
+            score: score
+        });
+        req.session.p52=null;
+        // session_unregister(req.session.p52.pyp);
+
+        //   }if(req.session.p52.pyp.length){
+     var score= req.session.p52.pyp-1;
+  res.render('quizzes/randomnomore',{score: score -1});
+    }
+    else{
+        req.session.p52.pyp.push(req.quiz.id);
+        res.render('quizzes/randomresult', {
+            quiz: req.quiz,
+            result: result,
+            answer: answer,
+            score: req.session.p52.pyp.length-1
+        });
+
     }
 
-    models.Quiz.count({where:{id:{$notIn:req.session.p52.resolved}}})
-
-        .then(function(count){
-            if(count==0){
-                res.render('quizzes/randonomore', {
-                    score: req.session.p52.resolved.length - 1,
-                    //session_unregister(resolved);
-                    }
-                )
-            }else {
-                var numeroRandom = (Math.floor(Math.random() * count));
-                return models.Quiz.findAll({
-                    where: {id: {$notIn: req.session.p52.resolved}},
-                    limit: 1,
-                    offset: numeroRandom
-
-                    });
-        }})
-        .then(function(quizzes){
-
-            res.render('quizzes/randomplay', {
-                quiz:quizzes[0],
-                score: req.session.p52.resolved.length-1
-            })
-
-        })
-
-
-
 };
-
